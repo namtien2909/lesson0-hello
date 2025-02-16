d693d63f03554779ddb34cccd08920772470a866
namtien2909 <24022819@vnu.edu.vn>
2025-02-16 20:40:17 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+double calculate(double num1, char operat, double num2) {
+    switch (operat) {
+    case '+':return num1 + num2; break;
+    case '-':return num1 - num2; break;
+    case '/':return num1 / num2; break;
+    case '*':return num1 * num2; break;
+    }
+}
